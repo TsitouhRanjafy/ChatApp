8abab5fd98f55ce8a182a3618d4bd4d4ecfcd809
TsitouhRanjafy <radohery09@gmail.com>
2025-01-31 10:25:43 +0100

    init UI, ecoute connexion client

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
-console.log("tesudht");
+import express from 'express';
+import { createServer } from 'node:http';
+import  path  from "node:path"
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+// Initialise une nouvelle instance de socket.io 
+// en passant le server (http) objet.
+const io = new Server(server)
+
+const cheminStatic = path.join(__dirname,'../public')
+
+app.use(express.static(cheminStatic))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(cheminStatic,'index.html'));
+});
+
+// ecoute le connection événement pour les sockets entrants et connectez-le à la console 
+io.on('connection', (socket) => {
+    console.log('as user connected');
+})
+
+server.listen(3000, () => {
+  console.log(' server running at http://localhost:3000');
+});
